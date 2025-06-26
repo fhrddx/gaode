54a9a0c7b7a114ca66930c3ab397e7ae38209801
SIGENPOWER\100760 <fanghairong@sigenergy.com>
2025-06-26 17:04:55 +0800

    1

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,6 +1,13 @@
-import GaoDeWorld from './Geode/GaoDeWorld';
-//import GeoWorld3 from './GeoWorld/GeoWorld3';
-//const dom: HTMLElement = document.querySelector('#earth-canvas');
 
-const world = new GaoDeWorld('earth-canvas');
-world.init();
+//案例一：three.js + 外部模型
+import GeoWorld3 from './GeoWorld/GeoWorld3';
+const dom: HTMLElement = document.querySelector('#earth-canvas');
+new GeoWorld3({
+  dom
+});
+
+
+//案例二：高德地图+three.js
+//import GaoDeWorld from './Geode/GaoDeWorld';
+//const world = new GaoDeWorld('earth-canvas');
+//world.init();
