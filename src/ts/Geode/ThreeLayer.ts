633334f82cc3cb5c8f78110b67d97b7f8c380d31
SIGENPOWER\100760 <fanghairong@sigenergy.com>
2025-06-26 11:37:54 +0800

    1

--- src/ts/Geode/ThreeLayer.ts
+++ src/ts/Geode/ThreeLayer.ts
@@ -10,8 +10,14 @@ export default class ThreeLayer{
   public renderer;
 
   private map;
+  private container;
 
   constructor(option: ThreeLayerOption){
+    //地图是必须项
+    if (option.map === undefined || option.map == null || typeof option.map.getContainer !== 'function'){
+      throw Error('config.map invalid');
+    }
+    this.map = option.map;
 
   }
 }
